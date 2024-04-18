1c68121a09d0dbeeb63e3201f867c9a655f00219
rizalshmbg <sihombingrizal18@gmail.com>
2024-04-18 10:46:59 +0700

    create index.js for export default img and svg

---
+++ src/assets/svg/index.js
@@ -0,0 +1,8 @@
+export { default as iconBlacklist } from './icon-blacklist.svg'
+export { default as iconFacebook } from './icon-facebook.svg'
+export { default as iconInstagram } from './icon-instagram.svg'
+export { default as iconPreview } from './icon-preview.svg'
+export { default as iconText } from './icon-text.svg'
+export { default as iconTwitter } from './icon-twitter.svg'
+export { default as logo } from './logo.svg'
+export { default as logoReact } from './react.svg'
