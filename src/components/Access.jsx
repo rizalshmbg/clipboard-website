85b0756f32f5938caaba84142040ebb6474a9194
rizalshmbg <sihombingrizal18@gmail.com>
2024-04-18 11:47:51 +0700

    replace directive and custom class

--- src/components/Access.jsx
+++ src/components/Access.jsx
@@ -5,9 +5,9 @@ const Access = () => {
 		<section id='access'>
 			<div className='section-container my-20'>
 				<h3>Access Clipboard Anywhere</h3>
-				<p className='max-w-3xl mx-auto mb-24 text-xl leading-6 text-center text-grayishBlue'>
-					Whether you’re on the go, or at your computer, you can access all your
-					Clipboard snippets in a few simple clicks.
+				<p className='section-content mb-24'>
+					Whether you&#39;re on the go, or at your computer, you can access all
+					your Clipboard snippets in a few simple clicks.
 				</p>
 				<img src={imgDevices} alt='devices' className='mx-auto' />
 			</div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
 					className='mx-auto my-16'
 				/>
 				<h3>A history of everything you copy</h3>
-				<p className='max-w-3xl mx-auto mb-10 text-2xl text-grayishBlue'>
+				<p className='section-content mb-10 text-2xl'>
 					Clipboard allows you to track and organize everything you copy.
 					Instantly access your clipboard on all your devices.
 				</p>
