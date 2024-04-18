e73c748bdaed804a3ad86678a6512e4c2bf9ec2e
rizalshmbg <sihombingrizal18@gmail.com>
2024-04-18 10:26:55 +0700

    apply custom class & directive class

--- src/components/BtnContainerHero.jsx
+++ src/components/BtnContainerHero.jsx
@@ -2,7 +2,7 @@ import BtnLink from './btn/BtnLink'
 
 const BtnContainerHero = () => {
 	return (
-		<div className='flex flex-col justify-center w-full space-y-6 text-xl text-white md:flex-row md:space-y-0 md:space-x-4'>
+		<div className='btn-container'>
 			<BtnLink
 				href={'#'}
 				className={
