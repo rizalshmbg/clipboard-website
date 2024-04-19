207b5bc58d0b9293bebbcd7243542d51d256ec4a
rizalshmbg <sihombingrizal18@gmail.com>
2024-04-19 11:09:50 +0700

    add bg-image for hero section, add id for logo, h3, and p

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,15 +3,19 @@ import BtnContainerHero from './BtnContainerHero'
 
 const Hero = () => {
 	return (
-		<section id='hero'>
+		<section
+			id='hero'
+			className='bg-[url("/src/assets/img/bg-header-desktop.png")] bg-no-repeat bg-contain'
+		>
 			<div className='section-container mb-40 pt-16'>
 				<img
 					src={logo}
 					alt='logo icon of clipboard'
 					className='mx-auto my-16'
+					id='logo-1'
 				/>
-				<h3>A history of everything you copy</h3>
-				<p className='section-content mb-10 text-2xl'>
+				<h3 id='title-1'>A history of everything you copy</h3>
+				<p id='title-2' className='section-content mb-10 text-2xl'>
 					Clipboard allows you to track and organize everything you copy.
 					Instantly access your clipboard on all your devices.
 				</p>
