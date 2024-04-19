c29f37ff690dc45f3472111cf9fa50b5dd4b7b66
rizalshmbg <sihombingrizal18@gmail.com>
2024-04-19 11:10:00 +0700

    complete animation for hero section

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,42 @@ import {
 	Snippets,
 	Supercharge,
 } from './components'
+import { useLayoutEffect, useRef } from 'react'
+import gsap from 'gsap'
 
 const App = () => {
+	const comp = useRef(null)
+
+	useLayoutEffect(() => {
+		let ctx = gsap.context(() => {
+			const t1 = gsap.timeline()
+			t1.from('#hero', {
+				xPercent: -100,
+				duration: 1.3,
+				delay: 0.3,
+			})
+				.from(['#logo-1', '#title-1', '#title-2', '#btn-container'], {
+					opacity: 0,
+					y: '+=30',
+					stagger: 0.5,
+				})
+				.from('#logo-1', {
+					rotate: -360,
+					stagger: 0.5,
+					duration: 1.3,
+				})
+				.to('#logo-1', {
+					scale: 1.2,
+					stagger: 0.5,
+					duration: 1,
+				})
+		}, comp)
+
+		return () => ctx.revert()
+	}, [])
+
 	return (
-		<>
+		<div className='relative' ref={comp}>
 			<Hero />
 			<Snippets />
 			<Features />
@@ -20,7 +52,7 @@ const App = () => {
 			<References />
 			<BottomCTA />
 			<Footer />
-		</>
+		</div>
 	)
 }
 
